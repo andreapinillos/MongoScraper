567105b739b069f295221702f0b2a3a70109e770
Andrea Pinillos <andreapinillos1@gmail.com>
2017-09-21 15:04:08 -0400

    push 410243238409237850984327534908567penis81324982734928

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
 // Requiring our Note and Article models
 // var Note = require("./models/Note.js");
 // var Article = require("./models/Article.js");
@@ -10,12 +11,26 @@ var mongoose = require("mongoose");
 // var request = require("request");
 // var cheerio = require("cheerio");
 // Set mongoose to leverage built in JavaScript ES6 Promises
-mongoose.Promise = Promise;
+// mongoose.Promise = Promise;
 
 var PORT = process.env.PORT || 3000;
 
 // Initialize Express
 var app = express();
+
+// Import routes and give the server access to them.
+var router = express.Router()
+require("./controllers/scraper_controller.js")(router)
+
+// Serve static content
+app.use(express.static(__dirname + "/public"));
+
+// Set Handlebars.
+
+app.engine("handlebars", expressHandlebars({
+	defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
 
 // Use morgan and body parser with our app
 app.use(logger("dev"));
@@ -23,20 +38,7 @@ app.use(bodyParser.urlencoded({
   extended: false
 }));
 
-// Import routes and give the server access to them.
-var router = express.Router()
-require("./controllers/scraper_controller.js")(router)
-
-app.use("/", routes);
-
-// Serve static content
-app.use(express.static(__dirname + "/public"));
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+app.use(router)
 
 var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
 // mongoose.connect("mongodb://heroku_6qwnq0tj:smpei6n9m46kgldfuen9lhau0r@ds141514.mlab.com:41514/heroku_6qwnq0tj");
