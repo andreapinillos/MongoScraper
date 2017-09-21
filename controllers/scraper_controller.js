965c5d995436bdffa0052407ebb64c5666811662
Andrea Pinillos <andreapinillos1@gmail.com>
2017-09-21 14:52:51 -0400

    push 40002312

--- controllers/scraper_controller.js
+++ controllers/scraper_controller.js
@@ -1,11 +1,11 @@
 var express = require("express");
-var router = express.Router();
+// var router = express.Router();
 var request = require("request");
 var cheerio = require("cheerio");
 var mongoose = require("mongoose");
 
 // Set mongoose to leverage built in JavaScript ES6 Promises
-mongoose.Promise = Promise;
+// mongoose.Promise = Promise;
 
 var Note = require("./models/Note.js");
 var Article = require("./models/Article.js");
